d04f70027840c2904be3954957544b0174ef79f6
Twystidceed <kjwacikowski@gmail.com>
2019-06-04 03:12:29 -0400

    Update axe-setup-noswap.sh
    added clear screen

--- axe-setup-noswap.sh
+++ axe-setup-noswap.sh
@@ -80,6 +80,7 @@ fi
 
 genkey=$3
 #Enter the new BLS genkey
+clear
 echo -e "${YELLOW}AXE Coin DIP003 Masternode Setup Script V1 for Ubuntu 16.04 LTS${NC}"
 	read -e -p "Enter your BLS key:" genkey3;
               read -e -p "Confirm your BLS key: " genkey4;
