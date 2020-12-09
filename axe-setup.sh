656077082fc0d4654158dad21ce62fb62b278838
Twystidceed <kjwacikowski@gmail.com>
2020-12-09 17:55:41 -0500

    Update axe-setup.sh

--- axe-setup.sh
+++ axe-setup.sh
@@ -47,10 +47,10 @@ function stop_daemon {
 
 
 
- if [[ $(lsb_release -d) == *18.04* ]]; then
-   UBUNTU_VERSION=18
+ if [[ $(lsb_release -d) == *16.04* ]]; then
+   UBUNTU_VERSION=16
 else
-   echo -e "${RED}You are not running Ubuntu 18.04, Installation is cancelled.${NC}"
+   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
    exit 1
 
 fi
